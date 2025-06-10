9347acf172c9bb3af5d053c74b4ee53b5319b5c0
Taylor Vann <brian.t.vann@gmail.com>
2025-06-10 14:44:48 -0700

    sync minimal events to draw with multiple colors

--- examples/sketch/mod.js
+++ examples/sketch/mod.js
@@ -1,1 +1,29 @@
-export {};
+import { SuperAction, SuperActionEvent } from "superaction";
+const _superAction = new SuperAction({
+    host: document,
+    connected: true,
+    eventNames: [
+        "input",
+        "change",
+        "pointerdown",
+        "pointerup",
+        "pointermove",
+    ],
+});
+const countEl = document.querySelector("canvas");
+addEventListener("#action", function (e) {
+    if (!(e instanceof SuperActionEvent))
+        return;
+    let { action } = e;
+    console.log(action);
+    if ("update_color" === action) {
+    }
+    if ("press_pen" === action) {
+    }
+    if ("lift_pen" === action) {
+    }
+    if ("move_pen" === action) {
+    }
+    if ("move_pen_across_canvas" === action) {
+    }
+});
