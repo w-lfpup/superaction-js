a114b07600dbd6f665f6861547f791c79354bcb1
Taylor Vann <brian.t.vann@gmail.com>
2026-04-15 15:35:19 -0700

    add jackrabbit tests

---
+++ tests/mod.ts
@@ -0,0 +1,7 @@
+import * as InstantiationTests from "./instantiation.tests.js";
+import * as InteractionTests from "./interaction.tests.js";
+
+export const testModules = [
+	InstantiationTests,
+	InteractionTests,
+];
