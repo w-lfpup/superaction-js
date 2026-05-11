25cc7643ed7e9019e64ad012b64dcf109ce71afa
Taylor Vann <brian.t.vann@gmail.com>
2026-05-11 15:12:50 -0700

    a better test that fails

--- tests/interaction.tests.js
+++ tests/interaction.tests.js
@@ -1,9 +1,34 @@
-import { log } from "@w-lfpup/jackrabbit/browser/dist/commands.js";
-function lilTest() {
-    log("howdy!");
-    return "we fail";
+import { elementClick, log, findElement } from "@w-lfpup/jackrabbit/browser/dist/commands.js";
+import { SuperAction } from "../dist/mod.js";
+async function testOneShotClick() {
+    let superAction = new SuperAction({
+        host: document,
+        eventNames: ["click"],
+    });
+    document.body.setHTMLUnsafe(`
+		<button click:"one_shot_action">hello!</button>
+	`);
+    superAction.connect();
+    let actionReceipt;
+    let cb = function (e) {
+        log("callback was evoked");
+        let { kind } = e.action;
+        actionReceipt = e.action;
+    };
+    document.addEventListener("#action", cb);
+    let buttonId = await findElement("button");
+    await log(`button id: ${buttonId}`);
+    if (!buttonId)
+        return "failed to query button element";
+    let clickResult = await elementClick(buttonId);
+    await log(`clickResult: ${clickResult}`);
+    document.removeEventListener("#action", cb);
+    superAction.disconnect();
+    if (actionReceipt)
+        return;
+    return "actionReceipt is undefined";
 }
-export const tests = [lilTest];
+export const tests = [testOneShotClick];
 export const options = {
     title: import.meta.url,
 };
