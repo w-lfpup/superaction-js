50f7b8795987b5dd68c0f62e6ec0afb594b60a28
Taylor Vann <brian.t.vann@gmail.com>
2026-06-08 15:49:59 -0700

    add tests to check event types on elements not documents

---
+++ tests/src/event_type_tests.ts
@@ -0,0 +1,49 @@
+import { elementClick, findElement } from "@w-lfpup/jackrabbit";
+import {
+	ActionEventInterface,
+	ActionInterface,
+	SuperAction,
+} from "../../dist/mod.js";
+
+let superAction: SuperAction;
+let actionReceipt: ActionInterface | undefined;
+let cb = function (e: ActionEventInterface) {
+	let { type } = e.action;
+	if ("one_shot_action" === type) actionReceipt = e.action;
+};
+
+function setup() {
+	superAction = new SuperAction({
+		host: document.body,
+		eventNames: ["click"],
+	});
+
+	document.body.setHTMLUnsafe(`
+		<button click:="one_shot_action">hello!</button>
+	`);
+	superAction.connect();
+	document.body.addEventListener("#action", cb);
+}
+
+async function testEventTypes() {
+	let buttonId = await findElement("button");
+	if (!buttonId) return "failed to query button element";
+
+	let clickResult = await elementClick(buttonId);
+	if (!clickResult) return "failed to click button element";
+
+	if (!actionReceipt) return "actionReceipt is undefined";
+	if (actionReceipt.event.type !== "click")
+		return "Event found was not a click event.";
+}
+
+function tearDown() {
+	superAction.disconnect();
+	document.body.removeEventListener("#action", cb);
+}
+
+export const tests = [setup, testEventTypes, tearDown];
+
+export const options = {
+	title: import.meta.url,
+};
--- tests/src/mod.ts
+++ tests/src/mod.ts
@@ -1,11 +1,13 @@
-import * as OneShotTests from "./quick_one_shot.tests.js";
+import * as OneShotTests from "./quick_one_shot_tests.js";
 import * as PropagationTests from "./propagation_tests.js";
 import * as FormSubmissionTests from "./form_tests.js";
 import * as InfixTests from "./infix_tests.js";
+import * as EventTypeTests from "./event_type_tests.js";
 
 export const testModules = [
 	OneShotTests,
 	PropagationTests,
 	FormSubmissionTests,
 	InfixTests,
+	EventTypeTests,
 ];
